a8144d6683e87ad619b16e3e9b5d89f1e96492f0
bagglerock <bagglerock@gmail.com>
2020-09-12 19:00:00 -0400

    chore: add notes to think about

--- backend/controllers/recipes.ts
+++ backend/controllers/recipes.ts
@@ -3,7 +3,12 @@ import { Request, Response } from 'express';
 import { spoonacularClient } from '../services/spoonacularClient';
 
 class RecipeController {
-  getByKeyword = async (_: Request, res: Response) => {
+  getByKeyword = async (req: Request, res: Response) => {
+    // not sure if the request should come in as an object or an already made string made of parameters
+    // if its an object, the params are hidden to the user in the url and cannot be shared as a link
+    // if its a string, then the params must be made in the front end and sent back here and have the key appended
+    console.log(req);
+
     try {
       const response = await spoonacularClient.getRecipes();
 
