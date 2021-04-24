3386abaa43cad6432846180ba1870bc638603fd5
bagglerock <bagglerock@gmail.com>
2021-04-24 13:39:11 -0400

    feat: add a tag to header for easy navigation to home

--- frontend/src/ui/app/Layout/Header/Header.tsx
+++ frontend/src/ui/app/Layout/Header/Header.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
 import './Header.scss';
 
-export const Header: React.FC = () => <div className="header">Header</div>;
+export const Header: React.FC = () => (
+  <div className="header">
+    <a href="/">Header</a>
+  </div>
+);
