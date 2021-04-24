4535e66a7b52cd72205d780cd5560b74e9d4f185
bagglerock <bagglerock@gmail.com>
2021-04-24 13:50:18 -0400

    refactor: move router stuff up to App above layout

--- frontend/src/ui/app/App.tsx
+++ frontend/src/ui/app/App.tsx
@@ -1,10 +1,16 @@
+import { createBrowserHistory } from 'history';
 import React from 'react';
+import { Router } from 'react-router';
 import { Layout } from 'ui/app/Layout/Layout';
 
 export const App: React.FC = () => {
+  const history = createBrowserHistory();
+
   return (
     <div className="h-100">
-      <Layout />
+      <Router history={history}>
+        <Layout />
+      </Router>
     </div>
   );
 };
