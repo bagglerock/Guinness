b8800f6aa44b2b0603f938c5bba64e01bb6321b1
bagglerock <bagglerock@gmail.com>
2021-07-11 01:38:57 -0400

    feat: add default values to SearchParameters

--- frontend/src/ui/pages/SearchResults/SearchParameters.ts
+++ frontend/src/ui/pages/SearchResults/SearchParameters.ts
@@ -1,8 +1,8 @@
 export class SearchParameters {
-  query: string;
-  pageNumber: number;
-  numExpected: number;
-  filters: string;
+  query: string = '';
+  pageNumber: number = 1;
+  pageLimit: number = 20;
+  filters: string = '';
 
   constructor(data: Partial<SearchParameters>) {
     Object.assign(this, data);
