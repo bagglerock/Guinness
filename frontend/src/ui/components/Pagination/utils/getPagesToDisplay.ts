72b3cd8cdf5a487e2043cb92ce4dff3b162bdf75
bagglerock <bagglerock@gmail.com>
2021-07-11 01:38:08 -0400

    refactor: rename variables in getPagesToDisplay

--- frontend/src/ui/components/Pagination/utils/getPagesToDisplay.ts
+++ frontend/src/ui/components/Pagination/utils/getPagesToDisplay.ts
@@ -1,16 +1,16 @@
-export const getPagesToDisplay = (pageCount: number, currentPage: number, adjacents: number) => {
-  const allPages = Array.from(Array(pageCount).keys());
+export const getPagesToDisplay = (pageCount: number, currentIndex: number, adjacents: number) => {
+  const allPages = [...Array(pageCount).keys()];
 
-  if (currentPage <= adjacents) {
+  if (currentIndex <= adjacents) {
     return allPages.slice(0, adjacents * 2 + 1);
   }
 
-  if (currentPage >= pageCount - adjacents) {
+  if (currentIndex >= pageCount - adjacents) {
     return allPages.slice(pageCount - (adjacents * 2 + 1));
   }
 
-  if (currentPage > adjacents && currentPage < pageCount - adjacents) {
-    return allPages.slice(currentPage - adjacents, currentPage + adjacents + 1);
+  if (currentIndex > adjacents && currentIndex < pageCount - adjacents) {
+    return allPages.slice(currentIndex - adjacents, currentIndex + adjacents + 1);
   }
 
   return [];
