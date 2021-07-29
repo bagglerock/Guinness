a8876b0257b0516936e3438462d5b304e537e4ad
bagglerock <bagglerock@gmail.com>
2021-07-29 08:33:35 -0400

    refactor: add scss file to ErrorView

--- frontend/src/ui/components/ErrorView/ErrorView.tsx
+++ frontend/src/ui/components/ErrorView/ErrorView.tsx
@@ -1,10 +1,9 @@
 import { Card } from '@blueprintjs/core';
 import React from 'react';
+import './ErrorView.scss';
 
 export const ErrorView = (): JSX.Element => (
   <div className="d-flex align-items-center justify-content-center h-100">
-    <Card style={{ backgroundColor: '#ffdddd', border: '1px solid red', width: '80%', textAlign: 'center' }}>
-      There was an error getting the data.
-    </Card>
+    <Card className="error-view">There was an error getting the data.</Card>
   </div>
 );
