a71acbbe3048bb1a7f74dc3db31c0c7266ef63c6
bagglerock <bagglerock@gmail.com>
2021-08-08 00:35:02 -0400

    feat: connect filters to API call to spoonacular

--- frontend/src/services/repositories/recipeRepository/recipeRepository.ts
+++ frontend/src/services/repositories/recipeRepository/recipeRepository.ts
@@ -1,4 +1,3 @@
-import { stringify } from 'query-string';
 import { mapRecipe } from 'services/repositories/recipeRepository/mappers/mapRecipe';
 import { mapRecipeSummaries } from 'services/repositories/recipeRepository/mappers/mapRecipeSummaries';
 import { Recipe } from 'services/repositories/recipeRepository/models/Recipe';
@@ -6,10 +5,11 @@ import { RecipeSummaries } from 'services/repositories/recipeRepository/models/R
 import { recipeClient } from 'services/utils/recipeClient/recipeClient';
 import { recipeRoutes } from 'services/utils/recipeClient/recipeRoutes';
 import { SearchParameters } from 'ui/types/SearchParameters/SearchParameters';
+import { makeSearchQuery } from 'ui/types/SearchParameters/utils/makeSearchQuery';
 
 class RecipeRepository {
   async getAllRecipes(parameters: SearchParameters): Promise<RecipeSummaries> {
-    const queryString = stringify(parameters);
+    const queryString = makeSearchQuery(parameters);
     const route = recipeRoutes.getByKeyword(queryString);
 
     const response = await recipeClient.getAllRecipes(route);
