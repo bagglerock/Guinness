61a64a2ee04edc550bf486ef1a0004801c8212a3
bagglerock <bagglerock@gmail.com>
2021-10-07 08:19:43 -0400

    feat: add dotenv for local development

--- backend/services/spoonacularClient.ts
+++ backend/services/spoonacularClient.ts
@@ -1,4 +1,7 @@
 import { AxiosInstance, default as axios } from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const SPOONACULAR_URL = 'https://api.spoonacular.com/';
 const SPOONACULAR_API_KEY = process.env.SPOONACULAR_API_KEY || '';
